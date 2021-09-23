4b2eb4f739023b252484370ecbd2ea1d2f43a3ee
fox_coder508 <mahmood.ademoye16@gmail.com>
2021-09-23 17:49:47 +0100

    added fetch error message

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,32 @@ import Bloglist from "./Bloglist";
 const Home = () => {
      const [blogs, setBlogs] = useState(null);
      const [isLoading, setIsLoading] = useState(true);
+     const [error, setError] = useState(null);
 
     useEffect( () => {
         setTimeout( () => {
-            fetch('http://localhost:8000/blogs')
+            fetch('http://localhost:800/blogs')
             .then( res => {
+                if (!res.ok){
+                    throw Error('Could not fetch data for resource'); 
+                }
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsLoading(false);
-            });
+                setError(null);
+            })
+            .catch( err => {
+                setError(err.message);
+                setIsLoading(false);
+            })
         },1000);
     }, []);
 
     return (  
         <div className = "home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...Please wait.</div>}
             {blogs && <Bloglist blogs = {blogs}  title = "My Blogs" />} 
         </div>
