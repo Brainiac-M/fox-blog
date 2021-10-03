76ccd33c795e49b063e6f416cb3dfe124f51c92f
fox_coder508 <mahmood.ademoye16@gmail.com>
2021-10-04 00:18:55 +0100

    body height changed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import NotFound from './NotFound';
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="app">
         <Navbar />
           <div className = "content">
             <Switch>
